24314227894bfa6d5cb9014cd02528907e53c40e
José A. Fernández <jaf.haponiuk@gmail.com>
2022-11-19 17:36:51 -0300

    Comment added.

--- app/src/main/java/com/example/doglistexample/DogsResponse.kt
+++ app/src/main/java/com/example/doglistexample/DogsResponse.kt
@@ -2,6 +2,7 @@ package com.example.doglistexample
 
 import com.google.gson.annotations.SerializedName
 
+//JSON to dataclass parse
 data class DogsResponse(
     @SerializedName("status") var status: String,
     @SerializedName("message") var images: List<String>
